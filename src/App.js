488e293774c68d458298d4a61b929a11445d37ae
ylu <ylu@ert.com>
2020-09-19 00:36:34 -0400

    init

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+
+// components
+import InputTodo from './components/InputTodo';
+import ListTodos from './components/ListTodos';
+function App() {
+  return (
+    <>
+      <div className="container">
+        <InputTodo />
+        <ListTodos />
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// https://www.youtube.com/watch?v=ldYcgPKEZC8
+// PERN Stack Course - Postgres, Express, React, and Node
